41f7e5ed6fc5ffe00035ae133c5a82a2bf20cee8
Paul Vavich <paulvav@gmail.com>
2022-04-08 15:16:19 +1000

    added heaaaps of commit statements

--- extension/scripts/ChromeHeadlessBrowser.js
+++ extension/scripts/ChromeHeadlessBrowser.js
@@ -61,19 +61,25 @@ class ChromeHeadlessBrowser {
   }
   async fetchData (url, sitemap, parentSelectorId, callback, scope) {
     try {
+      console.log("fetching data")
       const page = await this.pagePromise
       await this.loadUrl(url)
+      console.log("lodded url")
 
       const mainFrame = page.mainFrame()
 
+      console.log("mainframe done")
       // Maybe we don't need a context each time?
       const isolatedWorldInfo = await page._client.send('Page.createIsolatedWorld', {frameId: mainFrame._id, worldName: 'web-scraper-headless'})
+      console.log("isolated world")
       const executionContextId = isolatedWorldInfo.executionContextId
       const JsHandleFactory = page._frameManager.createJSHandle.bind(page._frameManager, executionContextId)
+      console.log("handle")
 
       const executionContext = new ExecutionContext(page._client, {id: executionContextId}, JsHandleFactory)
 
       const bundle = await contentSraperBundler.getBundle()
+      console.log('get bundle')
       await executionContext.evaluate(bundle)
       const message = {
         extractData: true,
@@ -88,6 +94,7 @@ class ChromeHeadlessBrowser {
           })
         })
       }, message)
+      console.log("got data")
       callback.call(scope, null, data)
     } catch (e) {
       console.log(e)
